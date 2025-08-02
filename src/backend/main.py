df6ac3e8ed069ca698be52cd8ff6f4eaf6b95b47
Chromiumore <parts.dima06@gmail.com>
2025-08-02 18:26:16 +0300

    the simplest crud read operations + list storage

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,9 +1,33 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
+
+
+class Soundtrack:
+    def __init__(self, name: str, author: str, track_length: int,
+                 listens: int, genres: list | tuple, album: str | None = None):
+        self.name = name
+        self.author = author
+        self.track_length = track_length
+        self.listens = listens
+        self.genres = genres
+        self.album = album
+
+
+data = [Soundtrack('Aria 5', 'Joe Fofo', 121, 40175, ['rock']),
+        Soundtrack('Cake Song', 'Untalanted baker', 307, 23, ['pop', 'rap'], 'Void'), ]
 
 
 @app.get('/')
 def index():
     return {'test': 'Hello World!'}
+
+
+@app.get('/music/get')
+def get():
+    return data
+
+
+@app.get('/music/get/{track_id}')
+def get_all(track_id: int):
+    return data[track_id]
