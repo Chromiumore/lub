b5950fae9c9b4d4fbeea8e17647cb17cf579e25b
Chromiumore <parts.dima06@gmail.com>
2025-08-16 21:59:00 +0300

    application factory

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI
 from .soundtracks import router as soundtracks_router
 
-app = FastAPI()
 
-app.include_router(soundtracks_router)
+def create_app() -> FastAPI:
+    app = FastAPI()
+    app.include_router(soundtracks_router)
+
+    return app
