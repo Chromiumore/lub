1c98be82de699bb8e4d877a5692629f60d6db8cc
Chromiumore <parts.dima06@gmail.com>
2025-08-21 22:31:29 +0300

    creating file storage directory if not exists

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+import os
 from .soundtracks import router as soundtracks_router
+from .config import Config
 
 
 def create_app() -> FastAPI:
+
+    os.makedirs(Config.load().files.path, exist_ok=True)
+
     app = FastAPI()
     app.include_router(soundtracks_router)
 
