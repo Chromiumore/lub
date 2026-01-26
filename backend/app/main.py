e8ed00d9ce00d61be3e148b72f367254e825272c
Chromiumore <parts.dima06@gmail.com>
2026-01-27 01:43:57 +0300

    api v1 prefix

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,13 +4,14 @@ from .soundtracks import router as soundtracks_router
 from .auth import router as auth_router
 from .config import Config
 
+API_V1_PREFIX = '/api/v1'
 
 def create_app() -> FastAPI:
 
     os.makedirs(Config.load().files.path, exist_ok=True)
 
     app = FastAPI()
-    app.include_router(soundtracks_router)
-    app.include_router(auth_router)
+    app.include_router(soundtracks_router, prefix=API_V1_PREFIX)
+    app.include_router(auth_router, prefix=API_V1_PREFIX)
 
     return app
