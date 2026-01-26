a04724f7a938b720e527f04fc3840d33f09bf8bd
Chromiumore <parts.dima06@gmail.com>
2026-01-26 21:17:29 +0300

    padding mini player

--- frontend/lub/lib/features/player/presentation/widgets/mini_player.dart
+++ frontend/lub/lib/features/player/presentation/widgets/mini_player.dart
@@ -21,6 +21,7 @@ class _MiniPlayerState extends State<MiniPlayer> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(20),
       child: Column(
         children: [
           Slider(
