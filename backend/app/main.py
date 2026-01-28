8bc0196c2cf69bde2e026370f2d5361c836921c6
Chromiumore <parts.dima06@gmail.com>
2026-01-28 23:05:28 +0300

    auth exceptions global handler

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
 import os
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from authx.exceptions import AuthXException
 from .soundtracks import router as soundtracks_router
 from .auth import router as auth_router
 from .config import Config
+
 
 API_V1_PREFIX = '/api/v1'
 
@@ -13,5 +16,15 @@ def create_app() -> FastAPI:
     app = FastAPI()
     app.include_router(soundtracks_router, prefix=API_V1_PREFIX)
     app.include_router(auth_router, prefix=API_V1_PREFIX)
+    app.add_exception_handler(AuthXException, auth_exception_handler)
 
     return app
+
+
+async def auth_exception_handler(request: Request, exc: AuthXException):
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        content={"detail": f"Authentication error: {exc}"},
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
