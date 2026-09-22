92f70b9b065f1725fd65c79c5389c0544b2ce157
Chromiumore <parts.dima06@gmail.com>
2026-09-23 00:56:52 +0300

    async tests for login endpoint

--- backend/tests/api/auth/test_login.py
+++ backend/tests/api/auth/test_login.py
@@ -1,60 +1,60 @@
 from app.main import API_V1_PREFIX
 
 
-def test_login_success(client, default_user):
+async def test_login_success(client, default_user):
     user_data = default_user[1]
     payload = {
         'email': user_data.email,
         'password': user_data.password.get_secret_value()
     }
 
-    response = client.post(API_V1_PREFIX + '/login', json=payload)
+    response = await client.post(API_V1_PREFIX + '/login', json=payload)
 
     assert response.status_code == 200
     assert type(response.json().get('access_token')) is str
     assert type(response.json().get('refresh_token')) is str
 
-def test_login_not_exists(client, default_user):
+async def test_login_not_exists(client, default_user):
     payload = {
         'email': 'johndoe@mail.com',
         'password': 'iamnewuser'
     }
 
-    response = client.post(API_V1_PREFIX + '/login', json=payload)
+    response = await client.post(API_V1_PREFIX + '/login', json=payload)
 
     assert response.status_code == 401
     assert response.json().get('detail') == 'Incorrect email or password'
 
-def test_login_wrong_password(client, default_user):
+async def test_login_wrong_password(client, default_user):
     user_data = default_user[1]
     payload = {
         'email': user_data.email,
         'password': user_data.password.get_secret_value() + '123123'
     }
 
-    response = client.post(API_V1_PREFIX + '/login', json=payload)
+    response = await client.post(API_V1_PREFIX + '/login', json=payload)
 
     assert response.status_code == 401
     assert response.json().get('detail') == 'Incorrect email or password'
 
-def test_login_required_fields_missing(client, default_user):
+async def test_login_required_fields_missing(client, default_user):
     user_data = default_user[1]
     payload = {
             'email': user_data.email
         }
     
-    response = client.post(API_V1_PREFIX + '/login', json=payload)
+    response = await client.post(API_V1_PREFIX + '/login', json=payload)
     assert response.status_code == 422
 
     payload = {
             'password': user_data.password.get_secret_value()
         }
-    response = client.post(API_V1_PREFIX + '/login', json=payload)
+    response = await client.post(API_V1_PREFIX + '/login', json=payload)
     assert response.status_code == 422
 
-    response = client.post(API_V1_PREFIX + '/login', json={})
+    response = await client.post(API_V1_PREFIX + '/login', json={})
     assert response.status_code == 422
 
-def test_login_empty_body(client, default_user):
-    response = client.post(API_V1_PREFIX + '/login')
+async def test_login_empty_body(client, default_user):
+    response = await client.post(API_V1_PREFIX + '/login')
     assert response.status_code == 422
